c2957c5b3065477ff13c790172abae358276d3bf
Abram31 <a447623993@gmail.com>
2022-11-27 12:54:10 +0300

    feat: add adaptive to welcome page, boards page

--- src/components/modules/Main/PageWithBoards/PageWIthBoards.tsx
+++ src/components/modules/Main/PageWithBoards/PageWIthBoards.tsx
@@ -1,4 +1,5 @@
 import { Button } from 'components/modules/common/Button/Button';
+import Preloader from 'components/modules/common/preloader/Preloader';
 import { URLS } from 'constants/constants';
 import { fetchRequest } from 'fetch/fetchRequest';
 import { useAppDispatch } from 'hooks/hooks';
@@ -18,6 +19,7 @@ export interface StateBoardProps {
 
 export const PageWIthBoards = () => {
   const [stateModalNewBoard, setStateModalNewBoard] = useState(false);
+  const [loading, setLoading] = useState(false);
   const dispatch = useAppDispatch();
   const boards = useSelector((state: StateData) => state.boarders);
 
@@ -46,12 +48,14 @@ export const PageWIthBoards = () => {
     (event.target as HTMLDivElement).id === 'modal_wrapper' && closeAddBoard();
   };
   useEffect(() => {
+    setLoading(true);
     fetchRequest({
       method: 'GET',
       token: localStorage.getItem('token')!,
       URL: URLS.boards,
     }).then((data) => {
       dispatch(setBoards(data));
+      setLoading(false);
     });
   }, [dispatch]);
 
@@ -79,9 +83,14 @@ export const PageWIthBoards = () => {
               );
             })}
         </div>
+        {loading && <Preloader />}
       </section>
       {stateModalNewBoard && (
-        <ModalWindowNewBoard onClick={onClickCloseAddBoard} closeModal={closeAddBoard} />
+        <ModalWindowNewBoard
+          onClick={onClickCloseAddBoard}
+          closeModal={closeAddBoard}
+          setLoading={setLoading}
+        />
       )}
     </>
   );
